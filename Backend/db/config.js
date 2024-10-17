d21b35d14556979ab48b3ff007d28217092cba2e
AbeloByte <gobeztech20@gmail.com>
2024-10-17 13:02:26 -0400

    HOST added in to the env file

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -3,7 +3,7 @@ const mysql2 = require("mysql2");
 const dbconnection = mysql2.createPool({
   user: process.env.USER,
   database: process.env.DATABASE,
-  host: "localhost",
+  host: process.env.HOST,
   password: process.env.PASSWORD,
   connectionLimit: 10,
 });
